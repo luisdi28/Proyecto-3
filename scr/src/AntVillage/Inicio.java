14176570e170217ea3c71cbd6eb68b07a9b9f156
Pablo Sánchez <pablosanchez19@gmail.com>
2022-06-02 11:33:13 -0600

    Probando en la clase Inicio
    Se prueba los branches en dos clases diferentes

--- scr/src/AntVillage/Inicio.java
+++ scr/src/AntVillage/Inicio.java
@@ -5,5 +5,7 @@ public class Inicio {
     public static void main(String arg[]){
         System.out.println("Funciono");
         System.out.println("Se termino");
+        System.out.println("Probando en dos clases");
+        System.out.println("Hoy gana la sele");
     }
 }
