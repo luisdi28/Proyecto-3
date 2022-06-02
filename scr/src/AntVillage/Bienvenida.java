b1e172abd232f38e0405f7f3c26a0ac8a900ed16
Luis Diego Araya <luisdi.28@estudiantec.cr>
2022-06-02 11:33:44 -0600

    Pruebas

--- scr/src/AntVillage/Bienvenida.java
+++ scr/src/AntVillage/Bienvenida.java
@@ -5,5 +5,7 @@ public class Bienvenida {
         System.out.println("Prueba");
         System.out.println("Probando los branches");
         System.out.println("Saludos cordiales");
+        System.out.println("No funciono xd");
+        System.out.println("Hasta Luego");
     }
 }
