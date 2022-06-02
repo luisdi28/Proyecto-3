b937744a9c93bac2344ad7dda17b692f20a8b66c
Pablo Sánchez <pablosanchez19@gmail.com>
2022-06-02 11:27:11 -0600

    Probando los branches
    Se realiza una prueba de los branches

--- scr/src/AntVillage/Bienvenida.java
+++ scr/src/AntVillage/Bienvenida.java
@@ -3,5 +3,7 @@ package AntVillage;
 public class Bienvenida {
     public static void main(String arg[]) {
         System.out.println("Prueba");
+        System.out.println("Probando los branches");
+        System.out.println("Saludos cordiales");
     }
 }
