77664d5ecb33ba69ccca1813cabc668ea4eb36b2
Pablo Sánchez <pablosanchez19@gmail.com>
2022-06-04 03:59:02 -0600

    Se crea el método agregarArista
    Se crea el método para agregar aristas en la clase NodoG

--- scr/src/Graph/NodoG.java
+++ scr/src/Graph/NodoG.java
@@ -3,12 +3,44 @@ package Graph;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Nodo {
+public class NodoG {
 
+    // Atributos de la clase Nodo
     private String sector;
     private List<Arista> aristas;
 
-    public Nodo(String sector){
+    // Constructor de la clase Nodo
+    public NodoG(String sector){
         this.sector = sector;
+    }
+
+    // Métodos getter y setter
+    public String getSector() {
+        return sector;
+    }
+
+    public void setSector(String sector) {
+        this.sector = sector;
+    }
+
+    public List<Arista> getAristas() {
+        return aristas;
+    }
+
+    public void setAristas(List<Arista> aristas) {
+        this.aristas = aristas;
+    }
+
+    // Método para agregar una arista
+    //
+    public void agregarArista(Arista arista){
+
+        // Si la lista está vacía (null)
+        if (aristas == null) {
+            // Se crea un ArrayList por
+            aristas = new ArrayList<>();
+        }
+        // Si la lista no está vacía
+        aristas.add(arista);
     }
 }
