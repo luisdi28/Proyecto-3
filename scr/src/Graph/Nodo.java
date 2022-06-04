6ed22431e5e4147367d39332af38d26c36554e48
Pablo Sánchez <pablosanchez19@gmail.com>
2022-06-04 03:45:14 -0600

    Avance de la clase Nodo
    Se inicia con el desarrollo de la clase Nodo, creando atributos y el constructor

--- scr/src/Graph/Nodo.java
+++ scr/src/Graph/Nodo.java
@@ -1,4 +1,14 @@
 package Graph;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Nodo {
+
+    private String sector;
+    private List<Arista> aristas;
+
+    public Nodo(String sector){
+        this.sector = sector;
+    }
 }
