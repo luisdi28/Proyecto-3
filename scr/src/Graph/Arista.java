2988143ca8f043a63df83262c98611ebc891b5e3
Pablo Sánchez <pablosanchez19@gmail.com>
2022-06-04 10:53:30 -0600

    Avance en la clase Arista
    Se crean los atributos y el constructor de la clase Arista

--- scr/src/Graph/Arista.java
+++ scr/src/Graph/Arista.java
@@ -1,4 +1,19 @@
 package Graph;
 
 public class Arista {
+
+    // Atributos de la clase
+    private NodoG nodoOrigen;
+    private NodoG nodoDestino;
+    private int peso;
+
+    // Constructor de la clase Arista
+    public Arista(NodoG nodoOrigen, NodoG nodoDestino, int peso){
+
+        this.nodoOrigen = nodoOrigen;
+        this.nodoDestino = nodoDestino;
+        this.peso = peso;
+    }
+
+    
 }
