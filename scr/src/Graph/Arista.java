8bd4662ac24f86aab70c308fcfbc11bcbdc50161
Pablo Sánchez <pablosanchez19@gmail.com>
2022-06-04 10:57:11 -0600

    Creación de los métodos getter y setter de la clase Arista
    Se crean los método getter y setter en la clase Arista

--- scr/src/Graph/Arista.java
+++ scr/src/Graph/Arista.java
@@ -5,14 +5,39 @@ public class Arista {
     // Atributos de la clase
     private NodoG nodoOrigen;
     private NodoG nodoDestino;
-    private int peso;
+    private int distancia;
 
     // Constructor de la clase Arista
     public Arista(NodoG nodoOrigen, NodoG nodoDestino, int peso){
 
         this.nodoOrigen = nodoOrigen;
         this.nodoDestino = nodoDestino;
-        this.peso = peso;
+        this.distancia = peso;
+    }
+
+    // Métodos getter y setter
+    public NodoG getNodoOrigen() {
+        return nodoOrigen;
+    }
+
+    public void setNodoOrigen(NodoG nodoOrigen) {
+        this.nodoOrigen = nodoOrigen;
+    }
+
+    public NodoG getNodoDestino() {
+        return nodoDestino;
+    }
+
+    public void setNodoDestino(NodoG nodoDestino) {
+        this.nodoDestino = nodoDestino;
+    }
+
+    public int getDistancia() {
+        return distancia;
+    }
+
+    public void setDistancia(int distancia) {
+        this.distancia = distancia;
     }
 
     
