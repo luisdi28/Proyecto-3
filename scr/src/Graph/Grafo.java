5fbd4da1611f701e39ee5e449e39bdb3d7cdc608
Pablo Sánchez <pablosanchez19@gmail.com>
2022-06-05 00:18:32 -0600

    Avance en la clase Grafo
    Se crean el atributo y los métodos de la clase Grafo

--- scr/src/Graph/Grafo.java
+++ scr/src/Graph/Grafo.java
@@ -1,4 +1,25 @@
 package Graph;
 
+import java.util.List;
+
 public class Grafo {
+
+    // Atributo de la clase
+    private List<NodoG> nodos;
+
+    // Método que se encarga de agregar nodos al grafo
+    public void agregarNodo(NodoG nodoG){
+
+    }
+
+    // Método getter de la clase
+    public List<NodoG> getNodos(){
+
+    }
+
+    // Método que se encarga de retornar los nodos existentes en el grafo en formato String
+    @Override
+    public String toString(){
+
+    }
 }
