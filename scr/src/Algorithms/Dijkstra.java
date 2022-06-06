1ad6bf94f2724bf523079e00c2c9bfa1c3c31510
Pablo Sánchez <pablosanchez19@gmail.com>
2022-06-05 23:44:28 -0600

    Se definen los método necesarios para el algoritmo Dijkstra
    Se implementan métodos en la clase Dijkstra

--- scr/src/Algorithms/Dijkstra.java
+++ scr/src/Algorithms/Dijkstra.java
@@ -20,4 +20,28 @@ public class Dijkstra {
 
         this.grafo = grafo;
     }
+
+    // Método que se encarga de detectar la ruta más corta entre dos nodos
+    public int rutaCostoMinimo(NodoG origen, NodoG destino){
+
+        
+        return nodos.get(destino);
+    }
+
+    // Método que se encarga de retornar el nodo con la ruta registrada más corta
+    public NodoG obtenerMenor(){
+
+        Integer menor = Integer.MAX_VALUE;
+        Integer aux;
+        NodoG nodoMenor = null;
+
+
+        return nodoMenor;
+    }
+
+    // Método que se encarga de verificar si se han recorrido todos los nodos
+    public boolean recorridoCompleto(){
+
+        return true;
+    }
 }
