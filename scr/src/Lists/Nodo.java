b37081b1315fef27068d9a60a5652812d8d3964c
Luis S <ghaland550@gmail.com>
2022-06-06 11:08:48 -0600

    Creación parcial de la lista circular

--- scr/src/Lists/Nodo.java
+++ scr/src/Lists/Nodo.java
@@ -1,4 +1,25 @@
 package Lists;
 
 public class Nodo {
+
+    private String data;
+    private Nodo next;
+
+    public Nodo(String data){
+        this.next = null;
+        this.data = data;
+
+    }
+    public Object getData(){
+        return this.data; //cambiar a this.data si no sirve
+    }
+    public void setData(String data){
+        this.data = data;
+    }
+    public Nodo getNext(){
+        return this.next;
+    }
+    public void setNext(Nodo nodo){
+        this.next = nodo;
+    }
 }
--- scr/src/Lists/circularList.java
+++ scr/src/Lists/circularList.java
@@ -1,4 +1,50 @@
 package Lists;
 
 public class circularList {
+    private Nodo head;
+    private int Size;
+    private Nodo last;
+    public circularList(){
+        this.head = null;
+        this.Size = 0;
+        this.last =null;
+        //this.last.setnext = head , con cada iteracion al agregar un nuevo nodo
+    }
+    public void insertFirst(String data){
+        if (getTail() == null){
+            Nodo nuevonodo = new Nodo(data);
+            nuevonodo.setNext(head);
+            setHead(nuevonodo);
+            setTail(nuevonodo);
+            setSize(1);
+
+        }
+        else{
+            Nodo nuevonodo = new Nodo(data);
+            nuevonodo.setNext(getHead());
+            setHead(nuevonodo);
+            getTail().setNext(nuevonodo);
+            setSize(1);
+        }
+
+    }
+    public Nodo getTail(){
+        return this.last;
+    }
+    public void setTail(Nodo nodo) {
+        this.last = nodo;
+    }
+    public void setHead(Nodo nodo){
+        this.head = nodo;
+    }
+    public void getHead(Nodo nodo){
+        this.head= nodo;
+    }
+    public void setSize(int num){
+        this.Size +=num;
+    }
+    public Nodo getHead(){
+        return this.head;
+    }
+
 }
