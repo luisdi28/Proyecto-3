a46ea1e07ce4a7b39cf4c740b3d609361456d5f7
Pablo Sánchez <pablosanchez19@gmail.com>
2022-06-10 08:05:07 -0600

    Modificación en la clase Grafo
    Se crea el constructor de la clase Grafo y se comentan líneas de código

--- scr/src/Graph/Grafo.java
+++ scr/src/Graph/Grafo.java
@@ -1,7 +1,5 @@
 package Graph;
 
-import Lists.Nodo;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,6 +9,12 @@ public class Grafo {
     private static ArrayList<NodoG> nodos;
     // Tal vez se usará más adelante -> private ArrayList<NodoG> visitados;
 
+    // Constructor de la clase
+    public Grafo(){
+
+        nodos = new ArrayList<NodoG>();
+    }
+
     // Método que se encarga de agregar nodos al grafo
     public void agregarNodo(NodoG nodoG){
 
@@ -19,6 +23,8 @@ public class Grafo {
             // Se agrega el nodo al ArrayList
             nodos.add(nodoG);
         }
+
+        // En caso de que el nodo ya exista
         else{
             System.out.println("El nodo " + nodoG.toString() + " ya existe.");
         }
@@ -27,9 +33,13 @@ public class Grafo {
     // Método que se encarga de realizar la conexión entre dos nodos
     public void conectarNodos(NodoG nodoOrigen, NodoG nodoDestino, Arista arista){
 
+        // Condicional que conecta los nodos en caso de que el nodo destino no forme
+        // parte de los nodos adyacentes al nodo origen
         if (!nodoOrigen.getAdyacentes().containsKey(nodoDestino)){
             nodoOrigen.agregarArista(arista, nodoDestino);
         }
+
+        // En caso de que exista la conexión
         else{
             System.out.println("La conexión entre el nodo " + nodoOrigen + " y el nodo " + nodoDestino + " existe.");
         }
