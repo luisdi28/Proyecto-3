9a85f8b78ce15955108f4abc6e0ce1b575598d98
Luis S <ghaland550@gmail.com>
2022-06-10 11:01:10 -0600

    Completado y BUG lista circular

--- scr/src/Lists/circularList.java
+++ scr/src/Lists/circularList.java
@@ -23,7 +23,7 @@ public class circularList {
             Nodo nuevonodo = new Nodo(data);
             nuevonodo.setNext(getHead());
             setHead(nuevonodo);
-            getTail().setNext(head);
+            getTail().setNext(getHead());
             setSize(1);
         }
     }
@@ -58,9 +58,10 @@ public class circularList {
 
     public void displayList() {//arreglar este metodo
         Nodo current = getHead();
+        System.out.println("el head es:" + getTail().getNext().getData());
         while (current.getNext() != getHead()) {
             System.out.println(current.getData());
-            current = current.getNext();
+            current = current.getNext(); //al recorrer la lista hay un error
         }
         System.out.println(current.getData());
     }
@@ -73,6 +74,7 @@ public class circularList {
                 getTail().setNext(getHead());
                 //System.out.println(getTail().getNext().getData());
                 setSize(-1);
+                System.out.println("el tail es:" + getTail().getData());
             }
             else if (getTail().getData() == name){
                 Nodo current = getHead();
@@ -82,6 +84,7 @@ public class circularList {
                 current.setNext(current.getNext().getNext());
                 setTail(current);
                 setSize(-1);
+                System.out.println("el tail es:" + getTail().getData());
             }
             else{
                 Nodo current = getHead();
@@ -92,18 +95,43 @@ public class circularList {
                     counter +=1;
                     if (current.getNext().getData() == name){
                         current.setNext(current.getNext().getNext());
-                        setTail(current);
                         found ="encontrado";
                         break;
                     }
                 }
                 System.out.println(found);
                 setSize(-1);
+                System.out.println("el tail es:" + getTail().getData());
             }
 
         }
-        else{ // se debe crear un metodo en caso de que hayan 2 nodos o menos.
+        else{
+            if (getSize() ==2){
+                System.out.println("quedan 2 nodos");
+                System.out.println("el tail es:" + getTail().getData());
+                if (getHead().getData()== name) {
+                    System.out.println("se elimina el head");
+                    setHead(getHead().getNext());
+                    System.out.println("el nuevo head es:" + getHead().getData());
+                    //System.out.println(this.head.getData());
+                    setTail(getHead());
+                    System.out.println("el nuevo tail es:" + getTail().getData());
+                    getTail().setNext(getHead());
+                    //System.out.println(getTail().getNext().getData());
+                    setSize(-1);
+                }
+                else if (getTail().getData() == name){
+                    System.out.println("se elimina el tail");
+                    getHead().setNext(null);
+                    setSize(-1);
+                }
 
+            }
+            else{
+                System.out.println("se elimina la lista entera");
+                setHead(null);
+                setSize(-1);
+            }
         }
     }
 
@@ -120,7 +148,13 @@ public class circularList {
         circularList.delete("1"); //elimina el 1
         circularList.delete("3");
         circularList.displayList();
+        System.out.println("se va a eliminar el 4");
+        circularList.delete("2");
+        System.out.println("se va a elimino el 4");
         circularList.delete("4");
+        circularList.insertFirst("2");
+        circularList.insertFirst("3");
+        circularList.insertFirst("4");
         circularList.displayList();
 
     }
