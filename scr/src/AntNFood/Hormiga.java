3d363e1b6cc4c5a5d7d7381b2c05c847f4a98726
Pablo Sánchez <pablosanchez19@gmail.com>
2022-06-13 14:51:26 -0600

    Creación de métodos getter y setter
    Se crean los métodos getter y setter en la clase Hormiga

--- scr/src/AntNFood/Hormiga.java
+++ scr/src/AntNFood/Hormiga.java
@@ -31,5 +31,71 @@ public class Hormiga{
         this.image = imageIcon.getImage();
     }
 
+    // Métodos getter y setter
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public ArrayList getRecorridos() {
+        return recorridos;
+    }
+
+    public void setRecorridos(ArrayList recorridos) {
+        this.recorridos = recorridos;
+    }
+
+    public int getRecolectado() {
+        return recolectado;
+    }
+
+    public void setRecolectado(int recolectado) {
+        this.recolectado = recolectado;
+    }
+
+    public int getPosicion_x() {
+        return posicion_x;
+    }
+
+    public void setPosicion_x(int posicion_x) {
+        this.posicion_x = posicion_x;
+    }
+
+    public int getPosicion_y() {
+        return posicion_y;
+    }
+
+    public void setPosicion_y(int posicion_y) {
+        this.posicion_y = posicion_y;
+    }
+
+    public int getPosicion_dx() {
+        return posicion_dx;
+    }
+
+    public void setPosicion_dx(int posicion_dx) {
+        this.posicion_dx = posicion_dx;
+    }
+
+    public int getPosicion_dy() {
+        return posicion_dy;
+    }
+
+    public void setPosicion_dy(int posicion_dy) {
+        this.posicion_dy = posicion_dy;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+
     
 }
