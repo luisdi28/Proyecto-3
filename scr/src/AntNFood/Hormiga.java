827c1a780d9ed143fd0f2c7ff8e33d1b14a89b0c
Pablo Sánchez <pablosanchez19@gmail.com>
2022-06-13 14:46:06 -0600

    Desarrollo de la clase Hormiga
    Se crea el constructor de la clase Hormiga

--- scr/src/AntNFood/Hormiga.java
+++ scr/src/AntNFood/Hormiga.java
@@ -1,5 +1,6 @@
 package AntNFood;
 
+import javax.swing.*;
 import java.awt.Image;
 import java.util.ArrayList;
 
@@ -16,7 +17,19 @@ public class Hormiga{
     private int posicion_dy;            // Posicion dy de la imagen
     private Image image;                // Imagen
 
-    public Hormiga(){
+    // Constructor de la clase
+    public Hormiga(String tipo, int posicion_x, int posicion_y, int posicion_dx, int posicion_dy){
 
+        this.tipo = tipo;
+        this.recorridos = new ArrayList<>();
+        this.recolectado = 0;
+        this.posicion_x = posicion_x;
+        this.posicion_y = posicion_y;
+        this.posicion_dx = posicion_dx;
+        this.posicion_dy = posicion_dy;
+        ImageIcon imageIcon = new ImageIcon(this.getClass().getResource(tipo));
+        this.image = imageIcon.getImage();
     }
+
+    
 }
