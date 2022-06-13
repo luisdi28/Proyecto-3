98bf167c095db47d53c3eb8083ff81b6b9abaa66
Pablo Sánchez <pablosanchez19@gmail.com>
2022-06-13 14:34:30 -0600

    Avance en la clase Hormiga
    Se crean los atributos  necesarios para la clase Hormiga

--- scr/src/AntNFood/Hormiga.java
+++ scr/src/AntNFood/Hormiga.java
@@ -1,6 +1,22 @@
 package AntNFood;
 
-public class Hormiga {
+import java.awt.Image;
+import java.util.ArrayList;
 
+public class Hormiga{
 
+    // Atributos de la clase
+
+    private String tipo;                // Determina el tipo de hormiga (Azul o Verde) Ejemplo: "HormigaAzul.png"
+    private ArrayList recorridos;       // Cada índice cuenta con su respectivo numero de recorrido y los sectores
+    private int recolectado;            // Guarda cúanta comida ha recolectado
+    private int posicion_x;             // Posicion x de la imagen
+    private int posicion_y;             // Posicion y de la imagen
+    private int posicion_dx;            // Posicion dx de la imagen
+    private int posicion_dy;            // Posicion dy de la imagen
+    private Image image;                // Imagen
+
+    public Hormiga(){
+
+    }
 }
