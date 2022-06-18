52ca46cc24f6cc384370d07197ee642ac82dfb25
Pablo Sánchez <pablosanchez19@gmail.com>
2022-06-18 16:18:31 -0600

    Avance en la clase Archivo
    Se definen los atributos y el constructor de la clase

--- scr/src/ManejoDeArchivos/Archivo.java
+++ scr/src/ManejoDeArchivos/Archivo.java
@@ -1,6 +1,19 @@
 package ManejoDeArchivos;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
 public class Archivo {
 
-    
+    File file;
+    FileWriter fileWriter;
+    PrintWriter printWriter;
+    BufferedWriter bufferedWriter;
+
+    public Archivo(){
+
+        this.file = new File("PruebaTXT/Info.txt");
+    }
 }
