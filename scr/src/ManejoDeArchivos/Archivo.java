39afd3d902f2afe472de4d36288dd1aadc1aae25
Pablo Sánchez <pablosanchez19@gmail.com>
2022-06-18 16:36:48 -0600

    Creación del método escribir()
    Se crea el método escribir() en la clase Archivo

--- scr/src/ManejoDeArchivos/Archivo.java
+++ scr/src/ManejoDeArchivos/Archivo.java
@@ -1,19 +1,58 @@
 package ManejoDeArchivos;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.PrintWriter;
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Archivo {
 
+    // Atributos de la clase
     File file;
     FileWriter fileWriter;
     PrintWriter printWriter;
     BufferedWriter bufferedWriter;
 
+    // Constructor de la clase
     public Archivo(){
 
-        this.file = new File("PruebaTXT/Info.txt");
+        // Ruta y nombre del archivo
+        this.file = new File("Registro/Info.txt");
+    }
+
+    // Método que se encarga de escribir en el archivo de texto
+    public  void escribir(String texto){
+
+        // Si el archivo no existe
+        if(!file.exists()){
+
+            try{
+
+                // Crea el archivo
+                file.createNewFile();
+                fileWriter = new FileWriter(file, true);
+                printWriter = new PrintWriter(fileWriter);
+                printWriter.println(texto);
+                printWriter.close();
+                fileWriter.close();
+
+            }catch (IOException e){
+                Logger.getLogger(Archivo.class.getName()).log(Level.SEVERE, null, e);
+            }
+        }
+        // Si el archivo ya existe
+        else{
+
+            try{
+
+                fileWriter = new FileWriter(file, true);
+                printWriter = new PrintWriter(fileWriter);
+                printWriter.println(texto);
+                printWriter.close();
+                fileWriter.close();
+
+            }catch (IOException e){
+                Logger.getLogger(Archivo.class.getName()).log(Level.SEVERE, null, e);
+            }
+        }
     }
 }
