83adfd14760a704efc2b8b1d28317084fdf8ae81
Pablo Sánchez <pablosanchez19@gmail.com>
2022-06-19 09:00:19 -0600

    Finalización en la lógica de la clase Arista
    Se concluyen métodos con nuevas modificaciones

--- scr/src/Graph/Arista.java
+++ scr/src/Graph/Arista.java
@@ -1,22 +1,40 @@
 package Graph;
 
-
 public class Arista {
 
     // Atributo de la clase
     private int distancia;
+    private NodoG nodoOrigen;
+    private NodoG nodoDestino;
 
     // Constructor de la clase Arista
-    public Arista(int distancia){
+    public Arista(NodoG origen, NodoG destino, int distancia){
 
+        this.nodoOrigen = origen;
+        this.nodoDestino = destino;
         this.distancia = distancia;
+    }
+
+    // Método getter y setter del atributo nodoOrigen
+    public NodoG getNodoOrigen() {
+        return nodoOrigen;
+    }
+    public void setNodoOrigen(NodoG nodoOrigen) {
+        this.nodoOrigen = nodoOrigen;
+    }
+
+    // Método getter y setter del atributo nodoDestino
+    public NodoG getNodoDestino() {
+        return nodoDestino;
+    }
+    public void setNodoDestino(NodoG nodoDestino) {
+        this.nodoDestino = nodoDestino;
     }
 
     // Método getter y setter del atributo distancia
     public int getDistancia() {
         return distancia;
     }
-
     public void setDistancia(int distancia) {
         this.distancia = distancia;
     }
