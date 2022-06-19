49002aa5f8d6c5a4f2c9814efb0f611be48d7704
Pablo Sánchez <pablosanchez19@gmail.com>
2022-06-19 09:02:16 -0600

    Finalización de lógica en la clase Grafo
    Se concluye la lógica en la clase Grafo

--- scr/src/Graph/Grafo.java
+++ scr/src/Graph/Grafo.java
@@ -1,12 +1,12 @@
 package Graph;
 
-
 import java.util.ArrayList;
 
 public class Grafo {
 
     // Atributo de la clase
     private static ArrayList<NodoG> nodos;
+
     // Tal vez se usará más adelante -> private ArrayList<NodoG> visitados;
 
     // Constructor de la clase
@@ -23,25 +23,15 @@ public class Grafo {
             // Se agrega el nodo al ArrayList
             nodos.add(nodoG);
         }
-
-        // En caso de que el nodo ya exista
-        else{
-            System.out.println("El nodo " + nodoG.toString() + " ya existe.");
-        }
     }
 
     // Método que se encarga de realizar la conexión entre dos nodos
-    public void conectarNodos(NodoG nodoOrigen, NodoG nodoDestino, Arista arista){
+    public void conectarNodos(NodoG nodoOrigen, NodoG nodoDestino, AristaH arista){
 
         // Condicional que conecta los nodos en caso de que el nodo destino no forme
         // parte de los nodos adyacentes al nodo origen
-        if (!nodoOrigen.getAdyacentes().containsKey(nodoDestino)){
-            nodoOrigen.agregarArista(arista, nodoDestino);
-        }
-
-        // En caso de que exista la conexión
-        else{
-            System.out.println("La conexión entre el nodo " + nodoOrigen + " y el nodo " + nodoDestino + " existe.");
+        if (!nodoOrigen.getAdyacentesLista().contains(nodoDestino)){
+            nodoOrigen.agregarAristaNodos(arista, nodoDestino);
         }
     }
 
@@ -73,8 +63,9 @@ public class Grafo {
 
     // Método que se encarga de retornar la cantidad de nodos adyacentes que tiene el nodo dado
     public int grado(NodoG nodoG){
-        return nodoG.getAdyacentes().size();
+        return nodoG.getAdyacentesLista().size();
     }
+
     // Método que se encarga de mostrar los nodos adyacentes al nodo dado
     // Los muestra en un conjunto
     public String nodosAdyacentes(NodoG nodoG){
